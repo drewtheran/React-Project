4f92a8bdba85502843fa48e8d5d9ba20beaf7c59
Drew Theran <drew.theran@gmail.com>
2020-05-08 12:00:36 -0700

    Component Adjustment

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,22 +1,43 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-import CodeVenomInfo from './CodeVenomInfoComponent';
 
 class Directory extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            selectedAspect: null
         };
+    }
+
+    onAspectSelect(aspect) {
+        this.setState({selectedAspect: aspect});
+    }
+
+    renderSelectedAspect(aspect) {
+        if (aspect) {
+            return (
+                <Card>
+                    <CardImg top src={aspect.image} alt={aspect.name}/>
+                    <CardBody>
+                        <CardTitle>{aspect.name}</CardTitle>
+                        <CardText>{aspect.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+        return <div />; //this is for if there isn't an aspect to pass through the method--so there is no error returned.
     }
 
     render() {
         const directory = this.props.aspects.map (aspect => {
             return (
-                <div key={aspect.id} className="col">
-                    <img src={aspect.image} alt={aspect.name} />
-                    <h2>{aspect.name}</h2>
-                    <p>{aspect.description}</p>
+                <div key={aspect.id} className="col-md-5 m-5">
+                    <Card onClick={() => this.onAspectSelect(aspect)}>
+                        <CardImg width='100%' src={aspect.image} alt={aspect.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{aspect.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
                 </div>
             );
         });
@@ -25,6 +46,11 @@ class Directory extends Component {
             <div className="container">
                 <div className="row">
                     {directory}
+                </div>
+                <div className="row">
+                    <div className="col-md-5 m-1">
+                        {this.renderSelectedAspect(this.state.selectedAspect)}
+                    </div>
                 </div>
             </div>
         );
--- src/shared/aspects.js
+++ src/shared/aspects.js
@@ -2,21 +2,25 @@ export const ASPECTS = [
     {
         id: 0,
         name: 'Regions',
-        image: 'assets/img_regions/globe_regions.jpg'
+        image: 'assets/img_regions/globe_regions.jpg',
+        description: 'These are the regions we collect venom for medicinal research.'
     },
     {
         id: 1,
         name: 'Medicines',
-        image: 'assets/img_medicines/Captopril.jpg'
+        image: 'assets/img_medicines/Captopril.jpg',
+        description: 'Here are some medicines that have been developed through our work.'
     },
     {
         id: 2,
         name: 'Spiders',
-        image: 'assets/img_spiders/Tarantula.jpg'
+        image: 'assets/img_spiders/Tarantula.jpg',
+        description: 'These are spiders with venoms needed to develop the most effective painkillers.'
     },
     {
         id: 3,
         name: 'Snakes',
-        image: 'assets/img_snakes/Pygmy_Rattlesnake.jpg'
+        image: 'assets/img_snakes/Pygmy_Rattlesnake.jpg',
+        description: 'These are snakes with venoms needed to develop the most effective medical treatments.'
     }
 ]
